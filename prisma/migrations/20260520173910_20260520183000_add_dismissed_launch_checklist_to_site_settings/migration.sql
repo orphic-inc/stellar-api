afd1dda78c0222559cd2d4820a623f6b38e82fd0
Kyle OBrien <obrienk@webbhost.net>
2026-05-20 13:14:51 -0500

    Fix launch checklist migration order

--- prisma/migrations/20260520173910_20260520183000_add_dismissed_launch_checklist_to_site_settings/migration.sql
+++ prisma/migrations/20260520173910_20260520183000_add_dismissed_launch_checklist_to_site_settings/migration.sql
@@ -1,2 +1,2 @@
--- AlterTable
-ALTER TABLE "site_settings" ALTER COLUMN "dismissedLaunchChecklist" DROP DEFAULT;
+ALTER TABLE "site_settings"
+ADD COLUMN "dismissedLaunchChecklist" TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[];
